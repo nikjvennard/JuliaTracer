481dc73a444e1ab7b970ec474ab32cfdb2e453d3
Nik <nikjvennard@gmail.com>
2021-11-15 23:03:23 -0800

    Added bluring filter

---
+++ Filters.jl
@@ -0,0 +1,39 @@
+function blur(A)
+    for i in CartesianIndices(A)
+        if i[1]+1 > size(A,1) || i[2]+1 > size(A,2)
+            continue
+        end 
+        a = A[i[1],i[2]]
+        b = A[i[1],i[2]+1]
+        c = A[i[1]+1,i[2]]
+        d = A[i[1]+1,i[2]+1]
+        avg = (a+b+c+d)/4.0
+    
+        A[i[1],i[2]] = avg
+        A[i[1],i[2]+1] = avg
+        A[i[1]+1,i[2]] = avg
+        A[i[1]+1,i[2]+1] = avg
+    end
+return A
+end
+
+function blur(A, n)
+    for i in 1:n
+        for i in CartesianIndices(A)
+            if i[1]+1 > size(A,1) || i[2]+1 > size(A,2)
+                continue
+            end 
+            a = A[i[1],i[2]]
+            b = A[i[1],i[2]+1]
+            c = A[i[1]+1,i[2]]
+            d = A[i[1]+1,i[2]+1]
+            avg = (a+b+c+d)/4.0
+        
+            A[i[1],i[2]] = avg
+            A[i[1],i[2]+1] = avg
+            A[i[1]+1,i[2]] = avg
+            A[i[1]+1,i[2]+1] = avg
+        end
+    end
+return A
+end
